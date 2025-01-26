3f5925d90294d7013460ca5673d67f7fe870ed03
vlad1998arabadzhy <wladislaw8991@gmail.com>
2025-01-26 21:37:03 +0100

    Fixed KeyHandler issue

--- src/main/java/org/game/KeyHandler.java
+++ src/main/java/org/game/KeyHandler.java
@@ -13,31 +13,33 @@ public class KeyHandler implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        int code = e.getKeyCode();
-        System.out.println("Key pressed: " + code);
+        int key= e.getKeyChar();
 
-        if (code == KeyEvent.VK_W) {
-            upPressed = true;
-            System.out.println("Move!");
-        }
-        if (code == KeyEvent.VK_S) {
-            downPressed = true;
-        }
-        if (code == KeyEvent.VK_A) {
-            leftPressed = true;
-        }
-        if (code == KeyEvent.VK_D) {
-            rightPressed = true;
+        System.out.println("Key pressed: " + key);
+
+        switch (key) {
+            case KeyEvent.VK_W:
+                upPressed = true;
+                break;
+            case KeyEvent.VK_S:
+                downPressed = true;
+                break;
+            case KeyEvent.VK_A:
+                leftPressed = true;
+                break;
+            case KeyEvent.VK_D:
+                rightPressed = true;
+                break;
         }
 
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        int code = e.getKeyCode();
-        System.out.println("Key released: " + code);
+        int key= e.getKeyChar();
+        System.out.println("Key released: " + key);
 
-        switch (code) {
+        switch (key) {
             case KeyEvent.VK_W:
                 upPressed = false;
                 break;
